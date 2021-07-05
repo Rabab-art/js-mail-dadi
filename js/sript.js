420b951ae062abcd3c487c1187c58c4d6b49847b
Rabab-art <bouhamla.rabab@gmail.com>
2021-07-05 14:07:36 +0200

    Emails

--- js/sript.js
+++ js/sript.js
@@ -11,22 +11,49 @@
 
 
 // Dado
-var player1 = Math.floor(Math.random() * 6) + 1;
-var player2 = Math.floor(Math.random() * 6) + 1;
-var resultElement = document.getElementById(result);
+// var player1 = Math.floor(Math.random() * 6) + 1;
+// var player2 = Math.floor(Math.random() * 6) + 1;
+// var resultElement = document.getElementById('result');
 
-console.log('Tiro giocatore 1:', player1);
-console.log('Tiro giocatore 2:', player2);
+// console.log('Tiro giocatore 1:', player1);
+// console.log('Tiro giocatore 2:', player2);
 
-if (player1 > player2) {
-    console.log('Giocatore 1 ha vinto');
-    message = 'Giocatore 1 ha vinto';
-} else if (player2 > player1) {
-    console.log(' Giocatore 2 ha vinto');
-    message = 'Giocatore 2 ha vinto';
-} else {
-    console.log('Pareggio');
-    message = 'Pareggio';
+// if (player1 > player2) {
+//     console.log('Giocatore 1 ha vinto');
+//     message = 'Giocatore 1 ha vinto';
+// } else if (player2 > player1) {
+//     console.log(' Giocatore 2 ha vinto');
+//     message = 'Giocatore 2 ha vinto';
+// } else {
+//     console.log('Pareggio');
+//     message = 'Pareggio';
+// }
+// resultElementinnerText = message;
+
+//  Email
+var display = document.getElementById('display');
+var emails = [lola@libero.com,
+bibi@yahoo.it,
+balu@tiscali.it ];
+
+// collection email from user
+
+var userEmail = prompt('Inserisci la tua email', 'lola@libero.it');
+
+// Check
+
+var isAllowd = false;
+for (var i = 0; <emails.length; i ++) {
+    if (userEmail === emails[i]) {
+        console.log('userEmail', userEmail);
+        console.log('current Email checked from the list', emails)
+        isAllowd = true;
+    }
 }
 
-resultElement.innerText = message;
+if (emails) {
+    display.innerText = 'Benvenuto' + userEmail;
+} else {
+    display.innerText = 'Spiacente, non sei autorizzato';
+}
+}
